1f24b51080def91e42e10bdf653f05180f3230af
sjpokedoke <70572845+sjpokedoke@users.noreply.github.com>
2021-10-25 17:44:14 +0800

    Add files via upload

---
+++ average.py
@@ -0,0 +1,35 @@
+import csv
+import plotly.figure_factory as ff
+import pandas as pd
+import statistics
+import random
+
+df = pd.read_csv("data2.csv")
+data = df["average"].tolist()
+
+def randomsetofmean(count):
+    dataset = []
+    for i in range(0, count):
+        randomindex = random.randint(0, len(data)-1)
+        value = data[randomindex]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+def showfig(meanlist):
+    df = meanlist
+    mean = statistics.mean(meanlist)
+    stdev = statistics.stdev(meanlist)
+    print("Mean is: "+ str(mean))
+    print("Standard deviation is: "+ str(stdev))
+    fig = ff.create_distplot([df], ["Average"], show_hist = False)
+    fig.show()
+
+def setup():
+    meanlist = []
+    for i in range(0, 1000):
+        setofmeans = randomsetofmean(100)
+        meanlist.append(setofmeans)
+    showfig(meanlist)
+
+setup()
